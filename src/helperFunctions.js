9ca0336ffcfcbc9b8a1ef95c2b5a3ba7de59209b
suhasm98 <suhasmruthyunjaya98@gmail.com>
2023-01-18 04:20:26 +0530

    generate token logic

---
+++ src/helperFunctions.js
@@ -0,0 +1,20 @@
+const generateTokens = (numberOfTokens, tokensPerRow) => {
+    let numberOfRows = Math.ceil(10 / 4);
+    let outputTokensArray = [];
+    let tokenCount = 0;
+  
+    for (let i = 0; i < numberOfRows; i++) {
+      let tempTokens = [];
+      for (let j = 0; j < tokensPerRow; j++) {
+        tokenCount++;
+        if (tokenCount > numberOfTokens) break;
+        tempTokens.push(tokenCount);
+      }
+      outputTokensArray.push(tempTokens);
+    }
+  
+    return outputTokensArray;
+  };
+  
+  export default generateTokens;
+  
